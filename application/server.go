93700e5a5d67975062f0193ed1b0a3ffa1a3996e
Zahid Al Tair <mdzahidh119@gmail.com>
2019-03-11 18:41:17 +0600

    Add json

--- application/server.go
+++ application/server.go
@@ -1,25 +1,62 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+// Player represents a player's score information
+type Player struct {
+	Name  string
+	Score int
+}
+
 // PlayerStore stores players information
 type PlayerStore interface {
 	GetPlayerScore(string) (int, bool)
 	RegisterWin(string)
+	GetLeagueTable() []*Player
 }
 
 // PlayerServer serves players information
 type PlayerServer struct {
-	Store PlayerStore
+	store PlayerStore
+	http.Handler
+}
+
+// NewPlayerServer returns a new player server
+func NewPlayerServer(store PlayerStore) *PlayerServer {
+	svr := &PlayerServer{
+		store: store,
+	}
+
+	router := http.NewServeMux()
+	router.HandleFunc("/league", svr.leagueHandler)
+	router.HandleFunc("/players/", svr.scoreHandler)
+	svr.Handler = router
+
+	return svr
+}
+
+func (svr *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(svr.store.GetLeagueTable())
+	w.WriteHeader(http.StatusOK)
+}
+
+func (svr *PlayerServer) scoreHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		svr.serveScore(w, r)
+	case http.MethodPost:
+		svr.registerScore(w, r)
+	}
 }
 
 func (svr *PlayerServer) serveScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
-	score, found := svr.Store.GetPlayerScore(player)
+	score, found := svr.store.GetPlayerScore(player)
 	if !found {
 		w.WriteHeader(http.StatusNotFound)
 		return
@@ -29,18 +66,8 @@ func (svr *PlayerServer) serveScore(w http.ResponseWriter, r *http.Request) {
 
 func (svr *PlayerServer) registerScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
-	svr.Store.RegisterWin(player)
+	svr.store.RegisterWin(player)
 	w.WriteHeader(http.StatusAccepted)
-}
-
-// ServeHTTP serves players score
-func (svr *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		svr.serveScore(w, r)
-	case http.MethodPost:
-		svr.registerScore(w, r)
-	}
 }
 
 // InMemoryStore for fixed storage
@@ -64,8 +91,17 @@ func (s *InMemoryStore) RegisterWin(name string) {
 	s.store[name]++
 }
 
+// GetLeagueTable returns league table
+func (s *InMemoryStore) GetLeagueTable() []*Player {
+	league := []*Player{}
+	for name, score := range s.store {
+		league = append(league, &Player{name, score})
+	}
+	return league
+}
+
 func main() {
-	svr := &PlayerServer{NewInMemoryStore()}
+	svr := NewPlayerServer(NewInMemoryStore())
 	log.Println("Server starting on port: 8080")
 	if err := http.ListenAndServe(":8080", svr); err != nil {
 		log.Fatal("unable to serve", err)
--- application/server_test.go
+++ application/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -10,6 +11,7 @@ import (
 type StubPlayerStore struct {
 	Scores   map[string]int
 	WinCalls []string
+	league   []*Player
 }
 
 func (stub *StubPlayerStore) GetPlayerScore(name string) (int, bool) {
@@ -21,6 +23,35 @@ func (stub *StubPlayerStore) RegisterWin(name string) {
 	stub.WinCalls = append(stub.WinCalls, name)
 }
 
+func (stub *StubPlayerStore) GetLeagueTable() []*Player {
+	return stub.league
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
+func assertStatus(t *testing.T, got, want int) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got '%d' want '%d'", got, want)
+	}
+}
+func assertWinCalls(t *testing.T, got, want []string) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+}
+func assertLeague(t *testing.T, got, want []*Player) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got '%v' want '%v'", got, want)
+	}
+}
+
 func TestGetPlayers(t *testing.T) {
 	stub := &StubPlayerStore{
 		Scores: map[string]int{
@@ -28,7 +59,7 @@ func TestGetPlayers(t *testing.T) {
 			"floyd":  10,
 		},
 	}
-	svr := &PlayerServer{stub}
+	svr := NewPlayerServer(stub)
 
 	t.Run("Return Pepper's score", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodGet, "/players/pepper", nil)
@@ -63,7 +94,7 @@ func TestRegisterScore(t *testing.T) {
 			"floyd":  10,
 		},
 	}
-	svr := &PlayerServer{stub}
+	svr := NewPlayerServer(stub)
 	t.Run("Post Pepper's score", func(t *testing.T) {
 		request, _ := http.NewRequest(http.MethodPost, "/players/pepper", nil)
 		response := httptest.NewRecorder()
@@ -73,42 +104,59 @@ func TestRegisterScore(t *testing.T) {
 	})
 }
 
+func TestLeague(t *testing.T) {
+	league := []*Player{
+		{"Zahid", 20},
+		{"Al", 15},
+		{"Tair", 10},
+	}
+	stub := &StubPlayerStore{nil, nil, league}
+	svr := NewPlayerServer(stub)
+
+	t.Run("Retursn 200 on /league", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/league", nil)
+		response := httptest.NewRecorder()
+		svr.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+
+		var got []*Player
+		if err := json.NewDecoder(response.Body).Decode(&got); err != nil {
+			t.Fatal("unable to parse response body")
+		}
+		assertLeague(t, got, league)
+	})
+}
+
 func TestIntegration(t *testing.T) {
-	svr := PlayerServer{NewInMemoryStore()}
+	svr := NewPlayerServer(NewInMemoryStore())
 	t.Run("Post Pepper's score and get score", func(t *testing.T) {
 		requestPost, _ := http.NewRequest(http.MethodPost, "/players/pepper", nil)
 		requestGet, _ := http.NewRequest(http.MethodGet, "/players/pepper", nil)
-		response1 := httptest.NewRecorder()
-		response2 := httptest.NewRecorder()
 
 		svr.ServeHTTP(httptest.NewRecorder(), requestPost)
 		svr.ServeHTTP(httptest.NewRecorder(), requestPost)
 		svr.ServeHTTP(httptest.NewRecorder(), requestPost)
+
+		response1 := httptest.NewRecorder()
 		svr.ServeHTTP(response1, requestGet)
 		assertResponseBody(t, response1.Body.String(), "3")
 
 		svr.ServeHTTP(httptest.NewRecorder(), requestPost)
 		svr.ServeHTTP(httptest.NewRecorder(), requestPost)
+
+		response2 := httptest.NewRecorder()
 		svr.ServeHTTP(response2, requestGet)
 		assertResponseBody(t, response2.Body.String(), "5")
-	})
-}
 
-func assertResponseBody(t *testing.T, got, want string) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
-	}
-}
-func assertStatus(t *testing.T, got, want int) {
-	t.Helper()
-	if got != want {
-		t.Errorf("got '%d' want '%d'", got, want)
-	}
-}
-func assertWinCalls(t *testing.T, got, want []string) {
-	t.Helper()
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got '%v' want '%v'", got, want)
-	}
+		requestLeague, _ := http.NewRequest(http.MethodGet, "/league", nil)
+		response3 := httptest.NewRecorder()
+		svr.ServeHTTP(response3, requestLeague)
+		var got []*Player
+		if err := json.NewDecoder(response3.Body).Decode(&got); err != nil {
+			t.Fatal("unable to parse response body")
+		}
+		assertLeague(t, got, []*Player{
+			{"pepper", 5},
+		})
+	})
 }
